62ce1861fbdf6ff06f295d9eb337e9f4cb95fdef
Georgios Kaklamanos <gekaklam@gmail.com>
2016-05-17 15:50:34 +0200

    Added predictions. Created data frames with predictions and correct labels.

--- triple_ada.R
+++ triple_ada.R
@@ -3,6 +3,7 @@ library("data.table")
 
 var_names <- c("duration","protocol_type","service","flag","src_bytes","dst_bytes","land","wrong_fragment","urgent","hot","num_failed_logins","logged_in","num_compromised","root_shell","su_attempted","num_root","num_file_creations","num_shells","num_access_files","num_outbound_cmds","is_host_login","is_guest_login","count","srv_count","serror_rate","srv_serror_rate","rerror_rate","srv_rerror_rate","same_srv_rate","diff_srv_rate","srv_diff_host_rate","dst_host_count","dst_host_srv_count","dst_host_same_srv_rate","dst_host_diff_srv_rate","dst_host_same_src_port_rate","dst_host_srv_diff_host_rate","dst_host_serror_rate","dst_host_srv_serror_rate","dst_host_rerror_rate","dst_host_srv_rerror_rate")
 
+##############################################
 # Load the labeled training set and its labels
 train_labeled <- read.csv("../data/Train_LabeledSet.txt", header = FALSE, col.names = var_names)
 train_labeled_lbls <- read.csv("../data/Train_LabeledSet_Label.txt", header = FALSE , col.names = c("Type"))
@@ -11,13 +12,13 @@ train_labeled_lbls <- read.csv("../data/Train_LabeledSet_Label.txt", header = FA
 train_labeled <- as.data.table(train_labeled)
 train_labeled_lbls <- as.data.table(train_labeled_lbls)
 
-# Create an label that shows if it's an attack or not.
+# Create a label that shows if it's an attack or not.
 train_labeled_lbls[, "y" := ifelse(get("Type") == 1, 1, 2)]
 
 # Combine the objects
 train_cmb <- cbind(train_labeled,train_labeled_lbls[,.(y)])
 
-
+#################################################
 # Load the un labeled training set and its labels
 train_unlabeled <- read.csv("../data/Train_UnlabeledSet.txt", header = FALSE, col.names = var_names)
 train_unlabeled_lbls <- read.csv("../data/Train_UnlabeledSet_Label.txt", header = FALSE, col.names = c("Type"))
@@ -31,6 +32,7 @@ train_unlabeled_lbls[, "y" := ifelse(get("Type") == 1, 1, 2)]
 
 train_unl_cmb <- cbind(train_unlabeled,train_unlabeled_lbls[,.(y)])
 
+#################################################
 # Load the testing set and its labels
 test_set <- read.csv("../data/Testing_LabeledSet.txt", header = FALSE, col.names = var_names)
 test_set_lbls <- read.csv("../data/Testing_LabeledSet_Label.txt", header = FALSE, col.names = c("Type"))
@@ -44,21 +46,19 @@ test_set_lbls[, "y" := ifelse(get("Type") == 1, 1, 2)]
 
 test_cmb <- cbind(test_set,test_set_lbls[,.(y)])
 
+#################################################
 # Set the tree depth to 1 (stumps)
 # default <- rpart.control()
 
 stump <- rpart.control(cp = -1, maxdepth = 1, minsplit = 0)
 
 #Create the discrete ada boost object
-
 gdis <- ada(y~., data = train_cmb, iter = 50, loss = "e", type = "discrete", control = stump)
 
 #Create the real ada boost object
-
 greal <- ada(y~., data = train_cmb, iter = 50, loss = "e", type = "real", control = stump)
 
 #Create the gentle ada boost object
-
 ggen <- ada(y~., data = train_cmb, iter = 50, loss = "e", type = "gentle", control = stump)
 
 # Add test to gdis
@@ -74,4 +74,39 @@ ggen <- addtest(ggen, train_unl_cmb[, var_names, with=FALSE], train_unl_cmb[,y])
 pred_dis <- predict(gdis, test_set)
 pred_real <- predict(greal, test_set)
 pred_gent <- predict(ggen, test_set)
+
+# Convert predictions to data tables
+pred_dis_dt <- as.data.frame(pred_dis)
+pred_real_dt <- as.data.frame(pred_real)
+pred_gent_dt <- as.data.frame(pred_gent)
+
+# Combine the predictions with the correct labels
+dis_res <- cbind(pred_dis_dt[,"pred_dis"], test_set_lbls[,.(y)])
+real_res <- cbind(pred_real_dt[,"pred_real"], test_set_lbls[,.(y)])
+gent_res <- cbind(pred_gent_dt[,"pred_gent"], test_set_lbls[,.(y)])
+
+# Create a new column. If the prediction and the correct label match,
+# then put 1. Else put 0
+dis_res[, "p" := ifelse(get("V1")==get("y"), 1, 0)]
+real_res[, "p" := ifelse(get("V1")==get("y"), 1, 0)]
+gent_res[, "p" := ifelse(get("V1")==get("y"), 1, 0)]
+
+# Convert the columns to factors
+dis_res$y <- as.factor(dis_res$y)
+real_res$y <- as.factor(real_res$y)
+gent_res$y <- as.factor(gent_res$y)
+
+dis_res$p <- as.factor(dis_res$p)
+real_res$p <- as.factor(real_res$p)
+gent_res$p <- as.factor(gent_res$p)
+
+# Display the summary
+
+summary(dis_res)
+summary(real_res)
+summary(gent_res)
+
+
+
+
 
