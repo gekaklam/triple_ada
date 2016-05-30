c435b8674a69a21f71ac11ed756872a7ad641cca
Georgios Kaklamanos <gekaklam@gmail.com>
2016-05-30 03:43:14 +0200

    Added better print functionality. Added cmp_lbls function to compare labels

--- triple_ada.R
+++ triple_ada.R
@@ -1,6 +1,7 @@
 library("ada")
 library("data.table")
 
+#Rprof(filename = "Rprof.out", memory.profiling = TRUE )
 set.seed(123456)
 var_names <- c("duration","protocol_type","service","flag","src_bytes","dst_bytes","land","wrong_fragment","urgent","hot","num_failed_logins","logged_in","num_compromised","root_shell","su_attempted","num_root","num_file_creations","num_shells","num_access_files","num_outbound_cmds","is_host_login","is_guest_login","count","srv_count","serror_rate","srv_serror_rate","rerror_rate","srv_rerror_rate","same_srv_rate","diff_srv_rate","srv_diff_host_rate","dst_host_count","dst_host_srv_count","dst_host_same_srv_rate","dst_host_diff_srv_rate","dst_host_same_src_port_rate","dst_host_srv_diff_host_rate","dst_host_serror_rate","dst_host_srv_serror_rate","dst_host_rerror_rate","dst_host_srv_rerror_rate")
 
@@ -24,6 +25,10 @@ ss2 <- subset(train_cmb, y==2)
 train.ada1 <- rbind(ss1[1:1000], ss2[1:333])
 train.ada2 <- rbind(ss1[1001:2000], ss2[334:666])
 train.ada3 <- rbind(ss1[2001:3000], ss2[667:999])
+
+#train.ada1 <- rbind(ss1[1:333], ss2[1:333])
+#train.ada2 <- rbind(ss1[334:666], ss2[334:666])
+#train.ada3 <- rbind(ss1[667:999], ss2[667:999])
 
 train_dt <- list(train.ada1, train.ada2, train.ada3)
 type_i <- c("discrete", "real", "gentle")
@@ -95,63 +100,103 @@ MeasureError <- function(h_i, i) {
   n_inc <- sum(d_t1$V1 != d_t1$V2) + sum(d_t2$V3 != d_t2$V4)
   n_cor <- sum(d_t1$V1 == d_t1$V2) + sum(d_t2$V3 == d_t2$V4)
   er_i <- n_inc / n_cor
+#  er_i <- n_inc / (sum(dim(d_t1)[1] +dim(d_t2)[1]))
   return(er_i)
 }
 
 # Start of repeat
 repeat {
-  print(c("This is iter", iter))
+  cat("########################\n")
+  cat("# Start of iteration", iter, "#\n")
+  cat("########################\n\n")
+
   for (i in 1:3) {
     L_i[[i]] <- 0
     update_i[i] <- FALSE
     e_i[i] <- MeasureError(h_i[-i], i)
+    cat("Measured Error for", i, "is: \t",e_i[i],"\n")
+    cat("Previous Error for", i, "was: \t",e.n_i[i],"\n\n")
     if (e_i[i] < e.n_i[i]){
-      print(c("ei=",e_i[i],"en_i=",e.n_i[i]))
+      cat("Meassured Error is smaller than previous error. \n")
+      cat("Using an unlabeled data set of size:", dim(train.unlabeled)[1])
+      cat("\n")
+#      cat("Inside the first if","\n")
+#      cat("\n")
       # Here we name pred_1 = y, so that it is easier later in the rbind
-      y <- predict(h_i[-i][[1]], train.unlabeled)
+      y      <- predict(h_i[-i][[1]], train.unlabeled)
       pred_2 <- predict(h_i[-i][[2]], train.unlabeled)
+      
       d_t <- data.table(cbind(train.unlabeled, y))
       agree <- y == pred_2
+      cat("Number of predictions that classifier agree:", sum(agree==1), "\n")
       L_i[[i]] <- d_t[agree,]
+      
       if (l.n_i[i] == 0) {
-        print("lni=0")
+        cat("Previous length of L_",i," was 0", sep = "")
+        cat("\n")
+        cat("Classifier:",i,"has not been updated before.\n")
         l.n_i[i] <- floor( e_i[i] / ( e.n_i[i] - e_i[i] ) + 1 )
+        cat("Now length of L_",i," is set to: \t", l.n_i[i],"\n", sep = "")
       }
+      cat("\n")
       L.len <- dim(L_i[[i]])[1]
-      print(c("L.len", i, "is", L.len))
+      cat("Length of tmp_labeled set, for", i, "is now", L.len,"\n")
+      cat("\n")
       if (l.n_i[i] < L.len ) {
-        print(c("ln_i:",l.n_i[i],"L.len:",L.len))
-        print(c("e_i[i]*L.len", e_i[i]*L.len,"e.n_i[i]*l.n_i[i]",e.n_i[i]*l.n_i[i]))
-        print(c("update",i,update_i[i]))
-        print(c("l.n_i",i,"=", l.n_i[i]))
-        print(c("(e_i[i] / (e.n_i[i] - e_i[i]))", (e_i[i] / (e.n_i[i] - e_i[i]))))
+        cat("Previous length of tmp_labeled set, is less than new length. \n")
+        #cat("Inside third if. \n")
+        #cat("ln_i[",i,"] is:",l.n_i[i],"and L.len:",L.len, "\n")
+        #cat("\n")
+        #cat("e_i[i]*L.len", e_i[i]*L.len,"e.n_i[i]*l.n_i[i]",e.n_i[i]*l.n_i[i])
+        #cat("update",i,update_i[i])
+        #cat("l.n_i",i,"=", l.n_i[i])
+        #cat("(e_i[i] / (e.n_i[i] - e_i[i]))", (e_i[i] / (e.n_i[i] - e_i[i])))
         if (e_i[i]*L.len < e.n_i[i]*l.n_i[i]) {
+          cat("Eq 9, is not violated \n")
+          cat("Classifier: ", i, " is set to be updated. \n", sep = "")
+          #cat(e_i[i]*L.len, "is less than", e.n_i[i]*l.n_i[i], "\n")
           update_i[i] <- TRUE
-          print(c("update",i,update_i[i]))
+#          print(c("update",i,update_i[i]))
+          #cat("l[",i,"] is:", l.n_i[i], "\n")
+          cat("\n")
         } else if ( l.n_i[i] > (e_i[i] / (e.n_i[i] - e_i[i])) ) {
+            cat("Need to subsample tmp_labeled set. \n")
             s.len <- ceiling( (e.n_i[i]*l.n_i[i]) / e_i[i] - 1)
+            cat("Size of sample is equal to:", s.len, "\n")
             s.idx <- sample(1:L.len, s.len)
             L_i[[i]] <- L_i[[i]][s.idx,]
+            cat("Classifier:", i, "is set to be updated. \n")
             update_i[i] <- TRUE
+            cat("\n")
         }
       }
     }
   }
-  print(update_i)
+  cat("The classifiers to be updated are:",update_i,"\n\n")
   for (i in 1:3) {
     if (update_i[i] == TRUE) {
+      cat("Now updating:", i, "\n")
+      cat("\n")
+      cat("Dim, Train",i,"is:\t", dim(train_dt[[i]])[1], "\n")
+      cat("Dim, L_",i," is:\t\t ", dim(L_i[[i]])[1], "\n", sep = "")
+      cat("Dim, combined is: \t", dim(L_i[[i]])[1]+dim(train_dt[[i]])[1], "\n")
       h_i[[i]] <- ada(y~., data = rbind(train_dt[[i]],L_i[[i]]), iter = 50, loss="e", type = type_i[i], control = stump )
+      cat("Re-trained classifier with", h_i[[i]]$dim[1], "data points. \n")
+      cat("\n")
       e.n_i[[i]] <- e_i[[i]]
       l.n_i[[i]] <- dim(L_i[[i]])[1]
     } 
   }
-  print(iter)
+#  cat("# of iterations:",iter, "\n")
   if (update_i[1] != TRUE & update_i[2] !=TRUE & update_i[3] != TRUE) {
     break
   }
   else if (iter > 10) {
     break
   }
+  cat("######################\n")
+  cat("# End of iteration", iter, "#\n")
+  cat("######################\n\n")
   iter <- iter + 1
 }
 
@@ -165,21 +210,45 @@ tri_pred <- function(tst.dset, h_i) {
   return(tprd.agr$p)
 }
 
-tprd.cmb <- tri_pred(test.set, h_i)
-
-cmp_lbls <- function(lbls.1, lbls.2) {
-  crt <- sum(lbls.1 == lbls.2)
-  wrg <- sum(lbls.1 != lbls.2)
-  err <- wrg/crt
-  print(c("Correct: ", crt ))
-  print(c("Wrong: ",   wrg ))
-  print(c("Error: ",   err ))
+cmp_lbls <- function(pred.lbls, test.lbls) {
+  num.normal   <- sum(test.lbls == 1)
+  num.attack <- sum(test.lbls == 2)
+  true.pos <- sum((pred.lbls ==1) & (test.lbls == 1))
+  true.neg <- sum((pred.lbls ==2) & (test.lbls == 2))
+  false.pos <- sum((test.lbls == 1) & ( pred.lbls ==2 ))
+  false.neg <- sum((test.lbls == 2) & ( pred.lbls ==1 ))
+  #wrg <- sum(lbls.1 != lbls.2)
+  #err <- wrg/length(lbls.2)
+  num.cor  <- sum(pred.lbls == test.lbls)
+  pres <- num.cor / length(test.lbls) * 100
+#  rcl  <- num.cor / (num.cor + )
+  fpos <- num.miss.norm / num.normal * 100
+  dtrt <- num.det.att / num.attack * 100
+  #print(c("Correct: ", crt ))
+  #print(c("Wrong: ",   wrg ))
+  cat("True Pos:  \t", true.pos, "\n")
+  cat("True Neg:  \t", true.neg, "\n")
+  cat("False Pos: \t", false.pos, "\n")
+  cat("False Neg: \t", false.neg, "\n")
+  cat("normal:    \t", num.normal, "\n")
+  cat("attacks:   \t", num.attack, "\n")
+  cat("correct:   \t", num.cor, "\n")
+  cat("Precission: \t", pres, "\n")
+#  cat("Recall:     \t", rcl, "\n")
+  cat("False Posit:\t", fpos, "\n")
+  cat("Det Rate:   \t", dtrt ,"\n")
+  
 }
 
+#Rprof(filename = "Rprof.out", memory.profiling = TRUE )
+tprd.cmb <- tri_pred(test.set, h_i)
 cmp_lbls(tprd.cmb, test.set.lbls$y)
-cmp_lbls(tprd.1, test.set.lbls$y)
-cmp_lbls(tprd.2, test.set.lbls$y)
-cmp_lbls(tprd.3, test.set.lbls$y)
+table(tprd.cmb,test.set.lbls$y)
+#Rprof()
+
+#cmp_lbls(tprd.1, test.set.lbls$y)
+#cmp_lbls(tprd.2, test.set.lbls$y)
+#cmp_lbls(tprd.3, test.set.lbls$y)
 
 
 
